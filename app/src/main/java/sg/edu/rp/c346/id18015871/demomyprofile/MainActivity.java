3588f0b70ab6594b4def99d765a9758011313932
Jassherrin <18015871@myrp.edu.sg>
2020-07-06 16:57:43 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id18015871/demomyprofile/MainActivity.java
@@ -0,0 +1,59 @@
+package sg.edu.rp.c346.id18015871.demomyprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etName;
+    EditText etGPA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName.findViewById(R.id.editTextName);
+        etGPA.findViewById(R.id.editTextGPA);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Step 1a: Get the user input from the EditText and store it in a variable
+        String strName = etName.getText().toString();
+        String intGpa = etGPA.getText().toString();
+
+        //Step 1b: Obtain an instance of the SharedPreference Editor for update later
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        //Step 1c: Obtain an instance of the SharedPreference Editor for update later
+        SharedPreferences.Editor prefEdit =prefs.edit();
+
+        //Step 1d: Add the key-value pair
+        prefEdit.putString("name",strName);
+        prefEdit.putString("gpa",intGpa);
+
+        //Step 1e: Call commit() to save the changes into SharedPreferences
+        prefEdit.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Step 2a: Obtain an instance of SharedPreferences
+        SharedPreferences prefs =PreferenceManager.getDefaultSharedPreferences(this);
+
+        //Step 2b: Retrieve the saved data from the SharedPreferences object
+        String strName =prefs.getString("name","");
+        String intGpa = prefs.getString("gpa", "0");
+        //Step 2c: Update the UI element with the value
+        etName.setText(strName);
+
+    }
+}
